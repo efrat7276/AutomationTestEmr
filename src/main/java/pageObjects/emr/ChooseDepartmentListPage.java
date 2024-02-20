9bb488c9fa837483d3af9be392d9bd52d398dc66
bsd_efrat <efeldman@laniado.org.co.il>
2024-02-20 10:45:59 +0200

    fix chooseDepartment func in angular 14

--- src/main/java/pageObjects/emr/ChooseDepartmentListPage.java
+++ src/main/java/pageObjects/emr/ChooseDepartmentListPage.java
@@ -9,18 +9,10 @@ import java.util.List;
 
 public class ChooseDepartmentListPage {
 
-    @FindBy(how= How.XPATH , using = "//ngb-typeahead-window//button")
+    @FindBy(how= How.XPATH , using = "//ngb-typeahead-window[contains(@id,'ngb-typeahead')]/button/ngb-highlight")
     public List<WebElement> departmentList;
 
     @FindBy(how= How.ID , using = "dropdownBasic1")
     public WebElement btn_li_department;
 
-    @FindBy(how= How.XPATH , using = "//button[@class='btn btn-ok']")
-    public WebElement btn_ok;
-
-    @FindBy(how= How.XPATH , using = "//button[@class='btn btn-secondary']")
-    public WebElement btn_cancel;
-
-    @FindBy(how= How.XPATH , using = "//div[@class='patientsIcon']//span")
-    public WebElement box_patientNumber;
 }
--- src/main/java/workflows/departmentFlows.java
+++ src/main/java/workflows/departmentFlows.java
@@ -3,7 +3,6 @@ package workflows;
 import extensions.UIActions;
 import extensions.Verifications;
 import io.qameta.allure.Step;
-import org.openqa.selenium.WebElement;
 import utilities.CommonOps;
 
 public class departmentFlows extends CommonOps {
@@ -11,9 +10,7 @@ public class departmentFlows extends CommonOps {
     @Step("choose department")
     public static void chooseDepartment(String dep) throws InterruptedException {
 
-        //Thread.sleep(1000);
         UIActions.click(chooseDepartmentListPage.btn_li_department);
-
         for (int i = 0; i < chooseDepartmentListPage.departmentList.size(); i++) {
             if (chooseDepartmentListPage.departmentList.get(i).getText().contains(dep)) {
                 UIActions.click(chooseDepartmentListPage.departmentList.get(i));
