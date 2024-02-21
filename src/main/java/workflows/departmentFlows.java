10b84d5d5be03c07596edaf9348e83053c91fb7c
bsd_efrat <efeldman@laniado.org.co.il>
2024-02-21 12:20:30 +0200

    departmentFlows change func

--- src/main/java/workflows/departmentFlows.java
+++ src/main/java/workflows/departmentFlows.java
@@ -11,12 +11,18 @@ public class departmentFlows extends CommonOps {
     public static void chooseDepartment(String dep) throws InterruptedException {
 
         UIActions.click(chooseDepartmentListPage.btn_li_department);
-        for (int i = 0; i < chooseDepartmentListPage.departmentList.size(); i++) {
-            if (chooseDepartmentListPage.departmentList.get(i).getText().contains(dep)) {
-                UIActions.click(chooseDepartmentListPage.departmentList.get(i));
-                return;
-            }
-        }
+//        for (int i = 0; i < chooseDepartmentListPage.departmentList.size(); i++) {
+//            if (chooseDepartmentListPage.departmentList.get(i).getText().contains(dep)) {
+//                UIActions.click(chooseDepartmentListPage.departmentList.get(i));
+//            }
+//        }
+        chooseDepartmentListPage.departmentList.stream().forEach(i ->
+        {
+            if(i.getText().contains(dep))
+                {
+                    i.click();
+                }
+        });
         try {
             Thread.sleep(6000);
         }
