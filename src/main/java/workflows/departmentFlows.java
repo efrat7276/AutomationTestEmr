6538c406a921cb1ca21b47905ba6a844afa9dc34
bsd_efrat <efeldman@laniado.org.co.il>
2024-03-04 09:57:34 +0200

    departmentFlows fix func

--- src/main/java/workflows/departmentFlows.java
+++ src/main/java/workflows/departmentFlows.java
@@ -3,6 +3,7 @@ package workflows;
 import extensions.UIActions;
 import extensions.Verifications;
 import io.qameta.allure.Step;
+import org.openqa.selenium.WebElement;
 import utilities.CommonOps;
 
 public class departmentFlows extends CommonOps {
@@ -11,18 +12,13 @@ public class departmentFlows extends CommonOps {
     public static void chooseDepartment(String dep) throws InterruptedException {
 
         UIActions.click(chooseDepartmentListPage.btn_li_department);
-//        for (int i = 0; i < chooseDepartmentListPage.departmentList.size(); i++) {
-//            if (chooseDepartmentListPage.departmentList.get(i).getText().contains(dep)) {
-//                UIActions.click(chooseDepartmentListPage.departmentList.get(i));
-//            }
-//        }
-        chooseDepartmentListPage.departmentList.stream().forEach(i ->
-        {
-            if(i.getText().contains(dep))
-                {
-                    i.click();
-                }
-        });
+        for (int i = 0; i < chooseDepartmentListPage.departmentList.size(); i++) {
+            if (chooseDepartmentListPage.departmentList.get(i).getText().contains(dep)) {
+                 WebElement elem = chooseDepartmentListPage.departmentList.get(i);
+
+                UIActions.click(elem);
+            }
+        }
         try {
             Thread.sleep(6000);
         }
