3894d9e158b632b3295d6909806f4b7f5b839588
bsd_efrat <efeldman@laniado.org.co.il>
2024-03-07 15:05:17 +0200

    restore bug 2 - add func - take_pic_patientList

--- src/test/java/restoreBugs/restoreBugs2.java
+++ src/test/java/restoreBugs/restoreBugs2.java
@@ -19,20 +19,27 @@ import java.io.IOException;
 public class restoreBugs2 extends CommonOps {
 
 
-     @Test(description = "take picture cardex daily report at 23 o'clock ")
-     @Description("take picture cardex daily report at 23 o'clock ")
-     public void cardex_executeAndOpenDailyReport() throws InterruptedException, IOException {
+//     @Test(description = "take picture cardex daily report at 23 o'clock ")
+//     @Description("take picture cardex daily report at 23 o'clock ")
+//     public void cardex_executeAndOpenDailyReport() throws InterruptedException, IOException {
+//        WebFlows.login('n');
+//        Thread.sleep(6000);
+//        WebFlows.patientBoxEntry(9);
+//        Thread.sleep(6000);
+//        nurseFlows.executeAllToCurrentHourFor_daily_onceOnly_sos_weekly_byHourAfterApprovalNurse();
+//        nurseFlows.executionNurseSign();
+//        cardexPage.dailyReportIcon.click();
+//        Thread.sleep(1000);
+//        FileUtils.copyFile(utilities.Listeners.saveScreenshotFile(), new File("E:\\import\\AutomationProject_emr\\temp\\"+ Helpers.getFileName("clickDailyReport")+".png"));
+//       }
+
+    @Test(description = "take picture patient list ")
+    @Description("take picture patient list at 23 o'clock ")
+    public void take_pic_patientList() throws InterruptedException, IOException {
         WebFlows.login('n');
         Thread.sleep(6000);
-        WebFlows.patientBoxEntry(9);
-        Thread.sleep(6000);
-        nurseFlows.executeAllToCurrentHourFor_daily_onceOnly_sos_weekly_byHourAfterApprovalNurse();
-        nurseFlows.executionNurseSign();
-        cardexPage.dailyReportIcon.click();
-        Thread.sleep(1000);
-        FileUtils.copyFile(utilities.Listeners.saveScreenshotFile(), new File("E:\\import\\AutomationProject_emr\\temp\\"+ Helpers.getFileName("clickDailyReport")+".png"));
-       }
-
+        FileUtils.copyFile(utilities.Listeners.saveScreenshotFile(), new File("E:\\import\\AutomationProject_emr\\temp\\"+ Helpers.getFileName("patientList")+".png"));
+    }
 
 
 }
